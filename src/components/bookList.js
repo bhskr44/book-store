637e659dd89012d1c48593dcc7165bd84c7756ba
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-12-10 03:11:53 +0530

    Create bookList.js

---
+++ src/components/bookList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SingleBook from './singleBook';
+
+const BookList = () => {
+  return (
+    <div className="BookList">
+      <ul>
+        <SingleBook title="Alice In Wonderland" author="Lewis Carroll" />
+        <SingleBook title="Harry Porter" author="JK Rowlings" />
+        <SingleBook title="The Hobbit" author="JRR Tolkien" />
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
