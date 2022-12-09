d64529b6ffaa355a058c8b1d21cfb47643fcc23d
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-12-10 03:11:56 +0530

    Create categories.js

---
+++ src/components/categories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <button>Check Status</button>
+    </div>
+  );
+};
+
+export default Categories;
