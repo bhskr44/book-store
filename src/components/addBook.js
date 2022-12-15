e1516b0c131bcd188247108cecba98a371926b54
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-12-16 02:20:57 +0530

    Update addBook.js

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
-const AddBook = () => (
-  <div className="addBook">
-    <form>
-      <label htmlFor="title">
-        Title
-        <input type="text" id="title" placeholder="Title" />
-      </label>
-      <label htmlFor="author">
-        Author
-        <input type="text" id="author" placeholder="Author" />
-      </label>
-      <button type="button">Add Book</button>
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+
+  const addBook = (book) => {
+    dispatch({
+      type: 'add-book',
+      book,
+    });
+  };
+
+  const addBookHandler = () => {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const book = {
+      title,
+      author,
+    };
+    addBook(book);
+  };
+
+  return (
+    <div className="addBook">
+      <form>
+        <label htmlFor="title">
+          Title
+          <input type="text" id="title" placeholder="Title" />
+        </label>
+        <label htmlFor="author">
+          Author
+          <input type="text" id="author" placeholder="Author" />
+        </label>
+        <button type="button" onClick={addBookHandler}>
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
