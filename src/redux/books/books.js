ae2bf45587b30cab357dd655a4b673eac1e3aaf0
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-12-17 04:22:44 +0530

    Update books.js

---
+++ src/redux/books/books.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { uuid4 } from 'uuid4';
+
+const initialState = [];
+
+// Actions
+export const ADD = 'add-book';
+export const REMOVE = 'remove-book';
+export const GETBOOKS = 'get-books';
+export const SETBOOKS = 'set-books';
+
+// Action Creators
+export function addBook(newBook) {
+  return { type: ADD, newBook };
+}
+
+export function removeBook(id) {
+  return { type: REMOVE, id };
+}
+
+export function getBooks() {
+  return { type: GETBOOKS };
+}
+
+export function setBooks(books) {
+  return { type: SETBOOKS, books };
+}
+
+// Reducer
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD: {
+      const newBook = {
+        id: uuid4(),
+        title: action.book.title,
+        author: action.book.author,
+      };
+
+      axios({
+        method: 'post',
+        url: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/msW0hxnGD5Hf1orlt2iq/books',
+        headers: {},
+        data: {
+          item_id: newBook.id,
+          title: newBook.title,
+          category: 'All',
+          author: newBook.author,
+        },
+      });
+
+      return [...state, newBook];
+    }
+    case REMOVE: {
+      axios({
+        method: 'delete',
+        url: `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/msW0hxnGD5Hf1orlt2iq/books/${action.id}`,
+        headers: {},
+      });
+      return state.filter((widget) => widget.id !== action.id);
+    }
+    case GETBOOKS: {
+      return state;
+    }
+    case SETBOOKS: {
+      return action.books;
+    }
+    default:
+      return state;
+  }
+};
