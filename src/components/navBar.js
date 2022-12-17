5932af20ed6e2e763c3e11f7af7a259734582fc5
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-12-17 07:52:26 +0530

    Fix According to review

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,13 +1,19 @@
 import { NavLink } from 'react-router-dom';
+import PersonIcon from '@mui/icons-material/Person';
 import './css/navBar.css';
 
 const NavBar = () => (
   <header className="navbar">
-    <h1 className="nav_title">Book Store</h1>
-    <nav className="links_list">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/categories">Categories</NavLink>
-    </nav>
+    <div className="logo-menu">
+      <h1 className="nav_title">Bookstore CMS</h1>
+      <nav className="links_list">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/categories">Categories</NavLink>
+      </nav>
+    </div>
+    <div className="user">
+      <PersonIcon />
+    </div>
   </header>
 );
 
